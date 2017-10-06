327d39dc0d69278d78a272f48ad77e4736f4dd85
Unknown <bcmwow@gmail.com>
2017-10-06 16:36:57 -0400

    Auto stash before merge of "develop" and "origin/develop"
    added mediaplayer and mediaplaylist

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,9 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    mediaPlaylist(new QMediaPlaylist),
+    mediaPlayer(new QMediaPlayer)
 {
     ui->setupUi(this);
 }
@@ -11,4 +13,6 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete mediaPlaylist;
+    delete mediaPlayer;
 }
