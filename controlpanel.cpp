074b7e3e7dfd3f4ea0d5f63e12c1a200d5cd2cd3
Brian <bcmwow@gmail.com>
2017-10-26 17:45:47 -0400

    Time control
    infinite or up to 59:59
    fixed randomness slider

--- controlpanel.cpp
+++ controlpanel.cpp
@@ -1,5 +1,6 @@
 #include "controlpanel.h"
 #include "ui_controlpanel.h"
+#include <QLineEdit>
 
 ControlPanel::ControlPanel(QWidget *parent) :
     QWidget(parent),
@@ -8,6 +9,15 @@ ControlPanel::ControlPanel(QWidget *parent) :
     ui->setupUi(this);
 
     ui->randomnessLE->setValidator(new QDoubleValidator(0.00, 1.00, 2, this));
+    ui->minutesLE->setValidator(new QIntValidator(0, 59, this));
+    ui->secondsLE->setValidator(new QIntValidator(0, 59, this));
+
+    infinite = false;
+    mins = 0;
+    secs = 0;
+
+    QObject::connect(ui->minutesLE, SIGNAL(returnPressed()), SLOT(on_time_changed()));
+    QObject::connect(ui->secondsLE, SIGNAL(returnPressed()), SLOT(on_time_changed()));
 }
 
 ControlPanel::~ControlPanel()
@@ -15,11 +25,21 @@ ControlPanel::~ControlPanel()
     delete ui;
 }
 
+//return -1 if infinite, otherwise the length in seconds
+int ControlPanel::getTime(){
+    if(infinite) return -1;
+
+    return mins * 60 + secs;
+}
+
+double ControlPanel::getRandomness(){
+    return randomness;
+}
+
 void ControlPanel::on_randomnessLE_returnPressed()
 {
     ui->randomnessLE->clearFocus();
 }
-
 
 void ControlPanel::on_randomnessLE_editingFinished()
 {
@@ -27,9 +47,28 @@ void ControlPanel::on_randomnessLE_editingFinished()
     ui->randomnessSlider->setValue((int) (randomness * 100));
 }
 
-void ControlPanel::on_randomnessSlider_sliderMoved(int position)
+void ControlPanel::on_randomnessSlider_valueChanged(int value)
 {
-    randomness = (double)position / 100;
+    randomness = (double)value / 100;
     ui->randomnessLE->setText(QString::number(randomness));
 }
 
+
+void ControlPanel::on_infiniteSoundChk_toggled(bool checked)
+{
+    infinite = checked;
+
+    ui->minutesLE->setEnabled(!checked);
+    ui->secondsLE->setEnabled(!checked);
+}
+
+void ControlPanel::on_time_changed(){
+    mins = ui->minutesLE->text().toInt();
+    secs = ui->minutesLE->text().toInt();
+
+    QLineEdit* le = qobject_cast<QLineEdit*>(sender());
+    if(le->text().length() == 1){
+        le->setText("0" + le->text());
+    }
+    le->clearFocus();
+}
--- controlpanel.h
+++ controlpanel.h
@@ -15,14 +15,24 @@ public:
     explicit ControlPanel(QWidget *parent = 0);
     ~ControlPanel();
 
+    int getTime();
+    double getRandomness();
+
 private slots:
     void on_randomnessLE_returnPressed();
     void on_randomnessLE_editingFinished();
-    void on_randomnessSlider_sliderMoved(int position);
+    void on_randomnessSlider_valueChanged(int value);
+    void on_infiniteSoundChk_toggled(bool checked);
+    void on_time_changed();
+
 private:
     Ui::ControlPanel *ui;
     QString alg;
-    float randomness;
+    double randomness;
+
+    bool infinite;
+    int mins;
+    int secs;
 };
 
 #endif // CONTROLPANEL_H
