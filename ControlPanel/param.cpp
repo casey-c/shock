ae7bf9631480d85af9e2b5af58b52514e14fa74c
Shamus Cardon <shamuscardon@gmail.com>
2017-11-27 21:27:51 -0500

    more comments
    in the control panel section this time

--- ControlPanel/param.cpp
+++ ControlPanel/param.cpp
@@ -1,6 +1,6 @@
 #include "param.h"
 
-Param::Param(QString nm, double def, double lo, double hi, int precis){
+Param::Param(QString nm, double def, double lo, double hi, int precis){ // create a new param
     name = nm;
     value = def;
     low = lo;
@@ -12,46 +12,46 @@ void Param::addVisualElement(QWidget* w){
     elements.push_back(w);
 }
 
-void Param::addMutableElement(QSlider* w){
+void Param::addMutableElement(QSlider* w){ // add an element that the user can change
     MutableElement_slider* ms =
             new MutableElement_slider(w, low, high, precision, value);
 
     QObject::connect(ms, SIGNAL(sig_valueChanged(double)),
-                     this, SLOT(on_valueChanged(double)));
+                     this, SLOT(on_valueChanged(double))); // connect signals with proper slots
 
     QObject::connect(this, SIGNAL(changeValue(double)),
-                     ms, SLOT(on_valueChanged(double)));
+                     ms, SLOT(on_valueChanged(double))); // connect signals with proper slots
 
-    elements.push_back(w);
+    elements.push_back(w); // add element to elemt list
 }
 
-void Param::addMutableElement(QLineEdit* w){
+void Param::addMutableElement(QLineEdit* w){ // add an element that the user can change
     MutableElement_lineEdit* ms =
             new MutableElement_lineEdit(w, low, high, precision, value);
 
     QObject::connect(ms, SIGNAL(sig_valueChanged(double)),
-                     this, SLOT(on_valueChanged(double)));
+                     this, SLOT(on_valueChanged(double))); // connect signals with proper slots
 
     QObject::connect(this, SIGNAL(changeValue(double)),
-                     ms, SLOT(on_valueChanged(double)));
+                     ms, SLOT(on_valueChanged(double))); // connect signals with proper slots
 
-    elements.push_back(w);
+    elements.push_back(w); // add element to elemt list
 }
 
-void Param::addMutableElement(QCheckBox* w){
+void Param::addMutableElement(QCheckBox* w){ // add an element that the user can change
     MutableElement_checkBox* ms =
             new MutableElement_checkBox(w, low, high, value);
 
     QObject::connect(ms, SIGNAL(sig_valueChanged(double)),
-                     this, SLOT(on_valueChanged(double)));
+                     this, SLOT(on_valueChanged(double))); // connect signals with proper slots
 
     QObject::connect(this, SIGNAL(changeValue(double)),
-                     ms, SLOT(on_valueChanged(double)));
+                     ms, SLOT(on_valueChanged(double))); // connect signals with proper slots
 
-    elements.push_back(w);
+    elements.push_back(w); // add element to elemt list
 }
 
-const QList<QWidget*> Param::getElements(){
+const QList<QWidget*> Param::getElements(){ // returns all elements associated with parameter
     return elements;
 }
 
@@ -62,7 +62,7 @@ QString Param::toString(){
 void Param::on_valueChanged(double newval){
     value = newval;
 
-    emit changeValue(value);
+    emit changeValue(value); // tell linked slots that the value ws changed
 }
 
 
